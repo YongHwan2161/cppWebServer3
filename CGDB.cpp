05f843a977c30c9cc5b9cf6c5091805201c8d7e0
YongHwan2161 <116191051+YongHwan2161@users.noreply.github.com>
2025-01-05 14:22:38 +0000

    add channeled graph database.cpp

---
+++ CGDB.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+
+using uint = unsigned int;
+using ushort = unsigned short;
+using uchar = unsigned char;
+
+std::vector<uchar *> Node;
+
+uchar initValues[12] = {8, 0, 0, 0, 1, 0, 10, 0, 0, 0, 0, 0};
+void create_new_node(){
+    uchar *newNode = new uchar[12];
+    for (int i = 0; i < 12; ++i)
+    {
+        newNode[i] = initValues[i];
+    }
+}
+void create_DB()
+{
+    std::cout << "call create_DB()" << std::endl;
+    for (int i = 0; i < 256; ++i){
+        create_new_node();
+    }
+}
+int main(int argc, char const *argv[])
+{
+    /* code */
+    create_DB();
+    std::cout << "end of main()" << std::endl;
+    return 0;
+}
